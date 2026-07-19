0a013298a213fbc792929c3df31abcaa5756929f
Zarko Cesljarov <cesljarovzarkomh@gmail.com>
2026-07-19 10:27:36 +0200

    improve logging filtering

--- learning/example/object_recognition/application/api/print_logger.h
+++ learning/example/object_recognition/application/api/print_logger.h
@@ -3,16 +3,29 @@
 
 #include <iostream>
 
-#if defined(PRINT_LOGGER_ENABLE)
-#define PRINT_LOG(...)                                          \
-    do                                                          \
-    {                                                           \
-        std::cout << "[PROFILER] " << __VA_ARGS__ << std::endl; \
+#if defined(PRINT_LOGGER_ENABLE) || defined(PRINT_LOGGER_INFO_ENABLE)
+#define PRINT_INFO(...)                                     \
+    do                                                      \
+    {                                                       \
+        std::cout << "[INFO] " << __VA_ARGS__ << std::endl; \
     } while (0)
 #else
-#define PRINT_LOG(...) \
-    do                 \
-    {                  \
+#define PRINT_INFO(...) \
+    do                  \
+    {                   \
     } while (0)
-#endif // PRINT_LOGGER_ENABLE
+#endif // PRINT_LOGGER_INFO_ENABLE
+
+#if defined(PRINT_LOGGER_ENABLE) || defined(PRINT_LOGGER_ERROR_ENABLE)
+#define PRINT_ERROR(...)                                     \
+    do                                                       \
+    {                                                        \
+        std::cerr << "[ERROR] " << __VA_ARGS__ << std::endl; \
+    } while (0)
+#else
+#define PRINT_ERROR(...) \
+    do                   \
+    {                    \
+    } while (0)
+#endif // PRINT_LOGGER_ERROR_ENABLE
 #endif // PRINT_LOGGER_H_
